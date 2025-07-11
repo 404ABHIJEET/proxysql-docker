30b8c4edee66c205225345d04bcd8dc99221b909
ABHIJEET <abhijeet.kumar@avaloninfosys.com>
2025-07-11 10:01:51 +0530

    sc

---
+++ sqls/proxysql.sql
@@ -0,0 +1,48 @@
+-- updating monitor username and password
+UPDATE global_variables SET variable_value='monitor' WHERE variable_name='mysql-monitor_username';
+UPDATE global_variables SET variable_value='monitor' WHERE variable_name='mysql-monitor_password';
+LOAD MYSQL VARIABLES TO RUNTIME;
+SAVE MYSQL VARIABLES TO DISK;
+
+-- entries of mysql servers in server table
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, comment) 
+VALUES (10, 'mariadb-master', 3306, 'admin_master'); 
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, comment) 
+VALUES (11, 'mariadb-slave-1', 3306, 'admin_replica1'); 
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, comment) 
+VALUES (20, 'mariadb-state-ch-master', 3306, 'ch_master'); 
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, comment) 
+VALUES (21, 'mariadb-state-ch-slave-1', 3306, 'ch_replica1'); 
+
+LOAD MYSQL SERVERS TO RUNTIME;
+SAVE MYSQL SERVERS TO DISK;
+
+-- entry of user in user table
+INSERT INTO mysql_users (username, password, default_hostgroup) 
+VALUES ('appuser', 'appuser', 10);
+
+LOAD MYSQL SERVERS TO RUNTIME;
+SAVE MYSQL SERVERS TO DISK;
+
+-- entries of replication hostgroup
+INSERT INTO mysql_replication_hostgroups (writer_hostgroup, reader_hostgroup, check_type, comment)
+VALUES (10, 11, 'read_only', 'parakh_hpc_admin');
+INSERT INTO mysql_replication_hostgroups (writer_hostgroup, reader_hostgroup, check_type, comment)
+VALUES (20, 21, 'read_only', 'parakh_hpc_state_ch');
+
+LOAD MYSQL SERVERS TO RUNTIME;
+SAVE MYSQL SERVERS TO DISK;
+
+-- entries of query rules for routing
+INSERT INTO mysql_query_rules (rule_id, active, schemaname, match_pattern, destination_hostgroup, apply, comment) 
+VALUES (1, 1, 'parakh_hpc_admin', '^(SELECT|DESCRIBE|SHOW)', 11, 1, 'admin_read');
+INSERT INTO mysql_query_rules (rule_id, active, schemaname, match_pattern, destination_hostgroup, apply, comment) 
+VALUES (2, 1, 'parakh_hpc_state_ch', '^(SELECT|DESCRIBE|SHOW)', 21, 1, 'ch_read');
+INSERT INTO mysql_query_rules (rule_id, active, schemaname, match_pattern, destination_hostgroup, apply, comment) 
+VALUES (3, 1, 'parakh_hpc_admin', '^(INSERT|UPDATE|DELETE|REPLACE|CREATE)', 10, 1, 'admin_write');
+INSERT INTO mysql_query_rules (rule_id, active, schemaname, match_pattern, destination_hostgroup, apply, comment) 
+VALUES (4, 1, 'parakh_hpc_state_ch', '^(INSERT|UPDATE|DELETE|REPLACE|CREATE)', 20, 1, 'ch_write');
+
+LOAD MYSQL QUERY RULES TO RUNTIME;
+SAVE MYSQL QUERY RULES TO DISK;
+
